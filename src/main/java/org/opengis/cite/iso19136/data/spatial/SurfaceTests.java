f18922621cd3da9046da75c1ee7f69fcdca0ba2b
R. Martell <rjmartell@computer.org>
2015-04-13 14:05:58 -0700

    Set configfailurepolicy = "continue"; update dependencies.

--- src/main/java/org/opengis/cite/iso19136/data/spatial/SurfaceTests.java
+++ src/main/java/org/opengis/cite/iso19136/data/spatial/SurfaceTests.java
@@ -45,105 +45,104 @@ import org.w3c.dom.NodeList;
  */
 public class SurfaceTests extends DataFixture {
 
-    NodeList surfaceNodes;
-    List<QName> surfaceElems = new ArrayList<QName>();
+	NodeList surfaceNodes;
+	List<QName> surfaceElems = new ArrayList<QName>();
 
-    /**
-     * A configuration method ({@code BeforeClass}) that looks for gml:Surface
-     * elements in the GML document under test (including any elements in its
-     * substitution group). If none are found all test methods defined in the
-     * class will be skipped.
-     */
-    @BeforeClass(alwaysRun = true)
-    public void findSurfaces() {
-        Source data = new StreamSource(this.dataFile);
-        if (null != this.model) {
-            XSElementDeclaration gmlSurface = this.model.getElementDeclaration(
-                    GML32.ABSTRACT_SURFACE, GML32.NS_NAME);
-            List<XSElementDeclaration> surfaceDecls = XMLSchemaModelUtils
-                    .getElementsByAffiliation(this.model, gmlSurface);
-            for (XSElementDeclaration decl : surfaceDecls) {
-                this.surfaceElems.add(new QName(decl.getNamespace(), decl
-                        .getName()));
-            }
-        }
-        Map<String, String> namespaceBindings = new HashMap<String, String>();
-        String xpath = generateXPathExpression(this.surfaceElems,
-                namespaceBindings);
-        try {
-            this.surfaceNodes = (NodeList) XMLUtils.evaluateXPath(data, xpath,
-                    namespaceBindings, XPathConstants.NODESET);
-        } catch (XPathExpressionException xpe) { // won't happen
-            throw new RuntimeException(xpe);
-        }
-        if (this.surfaceNodes.getLength() == 0) {
-            throw new SkipException(
-                    "No surface elements (that substitute for gml:Surface) were found.");
-        }
-    }
+	/**
+	 * A configuration method ({@code BeforeClass}) that looks for gml:Surface
+	 * elements in the GML document under test (including any elements in its
+	 * substitution group). If none are found all test methods defined in the
+	 * class will be skipped.
+	 */
+	@BeforeClass
+	public void findSurfaces() {
+		Source data = new StreamSource(this.dataFile);
+		if (null != this.model) {
+			XSElementDeclaration gmlSurface = this.model.getElementDeclaration(
+					GML32.ABSTRACT_SURFACE, GML32.NS_NAME);
+			List<XSElementDeclaration> surfaceDecls = XMLSchemaModelUtils
+					.getElementsByAffiliation(this.model, gmlSurface);
+			for (XSElementDeclaration decl : surfaceDecls) {
+				this.surfaceElems.add(new QName(decl.getNamespace(), decl
+						.getName()));
+			}
+		}
+		Map<String, String> namespaceBindings = new HashMap<String, String>();
+		String xpath = generateXPathExpression(this.surfaceElems,
+				namespaceBindings);
+		try {
+			this.surfaceNodes = (NodeList) XMLUtils.evaluateXPath(data, xpath,
+					namespaceBindings, XPathConstants.NODESET);
+		} catch (XPathExpressionException xpe) { // won't happen
+			throw new RuntimeException(xpe);
+		}
+		if (this.surfaceNodes.getLength() == 0) {
+			throw new SkipException("No surface elements found in GML data.");
+		}
+	}
 
-    /**
-     * [{@code Test}] Verifies that a gml:Surface element has a valid CRS
-     * reference.
-     * 
-     * <h6 style="margin-bottom: 0.5em">Sources</h6>
-     * <ul>
-     * <li>ISO 19136, cl. 9.10, 10.1.3.2</li>
-     * <li>ISO 19107, cl. 6.2.2.17 (Coordinate Reference System association)</li>
-     * </ul>
-     */
-    @Test(description = "See ISO 19136: 9.10, 10.1.3.2; ISO 19107: 6.2.2.17")
-    public void surfaceHasValidCRS() {
-        for (int i = 0; i < this.surfaceNodes.getLength(); i++) {
-            Element geom = (Element) this.surfaceNodes.item(i);
-            GeometryAssert.assertValidCRS(geom);
-        }
-    }
+	/**
+	 * [{@code Test}] Verifies that a gml:Surface element has a valid CRS
+	 * reference.
+	 * 
+	 * <h6 style="margin-bottom: 0.5em">Sources</h6>
+	 * <ul>
+	 * <li>ISO 19136, cl. 9.10, 10.1.3.2</li>
+	 * <li>ISO 19107, cl. 6.2.2.17 (Coordinate Reference System association)</li>
+	 * </ul>
+	 */
+	@Test(description = "See ISO 19136: 9.10, 10.1.3.2; ISO 19107: 6.2.2.17")
+	public void surfaceHasValidCRS() {
+		for (int i = 0; i < this.surfaceNodes.getLength(); i++) {
+			Element geom = (Element) this.surfaceNodes.item(i);
+			GeometryAssert.assertValidCRS(geom);
+		}
+	}
 
-    /**
-     * [{@code Test}] Checks that the boundary of a surface is topologically
-     * correct. Each component of the surface boundary must be a simple closed
-     * curve (ring); that is, it must not self-intersect and it forms a cycle
-     * such that the end points are identical.
-     * 
-     * Furthermore, each interior ring must be covered by the surface delimited
-     * by the exterior boundary (the rings may touch at a tangent point).
-     * 
-     * <h6 style="margin-bottom: 0.5em">Sources</h6>
-     * <ul>
-     * <li>ISO 19136:2007, cl. 10.5.11.1: Ring, RingType, curveMember</li>
-     * <li>ISO 19136:2007, cl. 10.5.5: exterior, interior</li>
-     * <li>ISO 19107:2003, cl. 6.3.6: GM_Ring</li>
-     * <li>ISO 19107:2003, cl. 6.3.7.2: exterior, interior</li>
-     * </ul>
-     */
-    @Test(description = "See ISO 19136: 10.5.5, 10.5.11.1; ISO 19107: 6.3.6, 6.3.7.2")
-    public void validSurfaceBoundary() {
-        for (int i = 0; i < this.surfaceNodes.getLength(); i++) {
-            Element surface = (Element) this.surfaceNodes.item(i);
-            GeometryAssert.assertValidSurfaceBoundary(surface);
-        }
-    }
+	/**
+	 * [{@code Test}] Checks that the boundary of a surface is topologically
+	 * correct. Each component of the surface boundary must be a simple closed
+	 * curve (ring); that is, it must not self-intersect and it forms a cycle
+	 * such that the end points are identical.
+	 * 
+	 * Furthermore, each interior ring must be covered by the surface delimited
+	 * by the exterior boundary (the rings may touch at a tangent point).
+	 * 
+	 * <h6 style="margin-bottom: 0.5em">Sources</h6>
+	 * <ul>
+	 * <li>ISO 19136:2007, cl. 10.5.11.1: Ring, RingType, curveMember</li>
+	 * <li>ISO 19136:2007, cl. 10.5.5: exterior, interior</li>
+	 * <li>ISO 19107:2003, cl. 6.3.6: GM_Ring</li>
+	 * <li>ISO 19107:2003, cl. 6.3.7.2: exterior, interior</li>
+	 * </ul>
+	 */
+	@Test(description = "See ISO 19136: 10.5.5, 10.5.11.1; ISO 19107: 6.3.6, 6.3.7.2")
+	public void validSurfaceBoundary() {
+		for (int i = 0; i < this.surfaceNodes.getLength(); i++) {
+			Element surface = (Element) this.surfaceNodes.item(i);
+			GeometryAssert.assertValidSurfaceBoundary(surface);
+		}
+	}
 
-    /**
-     * [{@code Test}] Checks that the orientation of the surface boundary is
-     * consistent with the upward normal. In essence, the interior is always to
-     * the left of a boundary curve. All patches must be oriented in the same
-     * manner.
-     * 
-     * <h6 style="margin-bottom: 0.5em">Sources</h6>
-     * <ul>
-     * <li>ISO 19136:2007, cl. 10.5.10: SurfaceType, Surface</li>
-     * <li>ISO 19107:2003, cl. 6.3.17: GM_Surface</li>
-     * <li>ISO 19107:2003, cl. 6.4.34: GM_SurfacePatch</li>
-     * </ul>
-     */
-    @Test(description = "See ISO 19136: 10.5.10; ISO 19107: 6.3.17, 6.4.34")
-    public void validSurfaceOrientation() {
-        for (int i = 0; i < this.surfaceNodes.getLength(); i++) {
-            Element surface = (Element) this.surfaceNodes.item(i);
-            GeometryAssert.assertValidSurfaceOrientation(surface);
-        }
-    }
+	/**
+	 * [{@code Test}] Checks that the orientation of the surface boundary is
+	 * consistent with the upward normal. In essence, the interior is always to
+	 * the left of a boundary curve. All patches must be oriented in the same
+	 * manner.
+	 * 
+	 * <h6 style="margin-bottom: 0.5em">Sources</h6>
+	 * <ul>
+	 * <li>ISO 19136:2007, cl. 10.5.10: SurfaceType, Surface</li>
+	 * <li>ISO 19107:2003, cl. 6.3.17: GM_Surface</li>
+	 * <li>ISO 19107:2003, cl. 6.4.34: GM_SurfacePatch</li>
+	 * </ul>
+	 */
+	@Test(description = "See ISO 19136: 10.5.10; ISO 19107: 6.3.17, 6.4.34")
+	public void validSurfaceOrientation() {
+		for (int i = 0; i < this.surfaceNodes.getLength(); i++) {
+			Element surface = (Element) this.surfaceNodes.item(i);
+			GeometryAssert.assertValidSurfaceOrientation(surface);
+		}
+	}
 
 }
